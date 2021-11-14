2af41583ea46f19a1217e7f85cbe7ae14bf08fd8
Alessio Fallani alessio.fallani@uni.lu <afallani@access1.iris-cluster.uni.lux>
2021-11-14 16:24:23 +0100

    m

--- splitting.py
+++ splitting.py
@@ -8,13 +8,7 @@ from matplotlib import pyplot as plt
 seed=0
 torch.manual_seed(seed)
 
-
 df=pd.read_json('./dataset41537.json') #put the name of the json file with the right data
 config_num=int(len(df)/41537)
-""" prop_list=df.columns[df.columns != 'BoB']
-prop_list=list(prop_list)
-prop_list.remove('atom_numbers')
-prop_list.remove('positions') 
-prop_list=list(df.columns[df.columns != 'BoB'])#[0:30] #comment this if you want all the properties
- """
+
 df_tr,df_test=split_train_test(df,save_to_file=True)
